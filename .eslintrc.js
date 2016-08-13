d3837559911e128d5ec4c08acfe39df573d53dca
Casper von pfaler <anoadev@gmail.com>
2016-08-13 08:43:54 +0300

    Use Airbnb eslint preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
     "extends": "airbnb",
+    "installedESLint": true,
     "plugins": [
         "react"
-    ],
-    "rules": {
-        "no-use-before-define": ["error", { "functions": false, "classes": false }]
-    }
+    ]
 };
